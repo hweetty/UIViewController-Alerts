9539db75c89ab91d1dc0b6793c9729df32281d25
Jerry Yu <hweetty@gmail.com>
2016-10-30 18:04:54 -0400

    Update examples

--- UIViewController+Alerts/ViewController.swift
+++ UIViewController+Alerts/ViewController.swift
@@ -12,22 +12,33 @@ class ViewController: UIViewController {
 
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		simpleAlert()
+		alertWithButtons()
 	}
 
 	func simpleAlert() {
 		self.show(.alert, title: "The Sky is Blue", message: nil)
 	}
 
-	func twoActions() {
+	func alertWithButtons() {
+		self.show(.alert, title: "What's Your Favourite?", message: nil, cancelTitle: "Huh?", otherTitles: ["Lightning", "Thunderbolt"]) { (action, index) in
+			switch index {
+			case 1:
+				print("Lightning")
+			case 2:
+				print("Thunderbolt")
+			default:
+				print("Wireless")
+			}
+		}
+	}
+
+	func simpleActionsheet() {
 		let tyles: [(String, UIAlertActionStyle)] = [
 			("Blue Pill", .default),
 			("Red Pill", .destructive),
 		]
 		self.show(.actionSheet, title: "Chose One", message: nil, titleAndStyles: tyles) { (action, index) in
-			// The index corresponds to the order of the given actions above
-			print(["Blue", "Red"][index])
-			self.twoActions()
+			print(tyles[index].0)
 		}
 	}
 
